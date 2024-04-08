eb197941f3bcbf07a14a78eca314a5da0bb7f8cf
Qihang <qihangz.work@gmail.com>
2024-04-08 12:23:49 -0700

    fix interface

--- model.py
+++ model.py
@@ -151,6 +151,10 @@ class random_classifier(nn.Module):
         self.NUM_CLASSES = NUM_CLASSES
         self.fc = nn.Linear(3, NUM_CLASSES)
         print("Random classifier initialized")
+        # create a folder
+        if 'models' not in os.listdir():
+            os.mkdir('models')
+        torch.save(self.state_dict(), 'models/conditional_pixelcnn.pth')
     def forward(self, x, device):
         return torch.randint(0, self.NUM_CLASSES, (x.shape[0],)).to(device)
     
