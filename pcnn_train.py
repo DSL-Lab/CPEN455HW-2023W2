5151064af06842bf0a117c3bf7b827c95d1b7149
TayyibCh <tayyib.chohan@korotu.com>
2024-04-16 22:48:04 -0700

    cuda >:(

--- pcnn_train.py
+++ pcnn_train.py
@@ -15,6 +15,7 @@ import argparse
 from pytorch_fid.fid_score import calculate_fid_given_paths
 
 torch.cuda.empty_cache()
+print(torch.cuda.memory_summary(device=None, abbreviated=False))
 
 def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, mode = 'training'):
     if mode == 'training':
